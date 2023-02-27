51f8c2be051ba0433c346f2a1605bca073399e05
Paulo Campos <77108503+paulo-campos-57@users.noreply.github.com>
2023-02-27 20:03:17 -0300

    Create 1004.c

---
+++ iniciante/1004.c
@@ -0,0 +1,16 @@
+//1004 - Produto simples
+#include<stdio.h>
+
+int main(void) {
+	
+	int a, b;
+	int prod;
+	scanf("%d", &a);
+	scanf("%d", &b);
+	
+	prod = a * b;
+	
+	printf("PROD = %d\n", prod);
+	
+	return 0;
+}
