859e7f86f9e575c7deded560efaba5fe38950c9a
Paulo Campos <77108503+paulo-campos-57@users.noreply.github.com>
2023-02-27 20:00:58 -0300

    Create 1001.c

---
+++ iniciante/1001.c
@@ -0,0 +1,15 @@
+//1001 - Extremamente básico
+#include <stdio.h>
+ 
+int main() {
+    
+    int a, b, x;
+    scanf("%d", &a);
+    scanf("%d", &b);
+    
+    x = a + b;
+    
+    printf("X = %d\n", x);
+    
+    return 0;
+}
