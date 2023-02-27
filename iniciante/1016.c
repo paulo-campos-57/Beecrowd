f6b3e2c8d8fda8921b6c895b100d0a3f9c1e04fe
Paulo Campos <77108503+paulo-campos-57@users.noreply.github.com>
2023-02-27 20:11:28 -0300

    Create 1016.c

---
+++ iniciante/1016.c
@@ -0,0 +1,14 @@
+//1016 - Distância
+#include<stdio.h>
+
+int main() {
+	
+	int minutos, distancia;
+	scanf("%d", &minutos);
+	
+	distancia = minutos * 2;
+	
+	printf("%d minutos\n", distancia);
+	
+	return 0;
+}
