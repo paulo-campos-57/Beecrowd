39813b3d2fca484151d529a80f8dd4685b831613
Paulo Campos <77108503+paulo-campos-57@users.noreply.github.com>
2023-02-27 20:02:23 -0300

    Create 1003.c

---
+++ iniciante/1003.c
@@ -0,0 +1,15 @@
+//1003 - Soma simples
+#include<stdio.h>
+
+int main(void) {
+    int a, b, soma;
+    
+    scanf("%d", &a);
+    scanf("%d", &b);
+    
+    soma = a + b;
+    
+    printf("SOMA = %d\n", soma);
+    
+    return 0;
+}
